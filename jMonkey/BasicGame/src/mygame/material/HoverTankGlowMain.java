08c6434fe06c2f64709f0a3b22373b6c9e03db6b
Qinghai Meng <mengqhai@gmail.com>
2014-08-24 21:51:23 +0800

    experiment with lights

--- jMonkey/BasicGame/src/mygame/material/HoverTankGlowMain.java
+++ jMonkey/BasicGame/src/mygame/material/HoverTankGlowMain.java
@@ -1,26 +1,18 @@
 package mygame.material;
 
-import com.jme3.animation.AnimControl;
-import com.jme3.animation.Animation;
-import com.jme3.animation.SpatialTrack;
 import com.jme3.app.SimpleApplication;
-import com.jme3.light.AmbientLight;
-import com.jme3.light.DirectionalLight;
+import com.jme3.light.SpotLight;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.filters.BloomFilter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
-import com.jme3.texture.Texture.WrapMode;
-import java.util.HashMap;
 
 /**
  * test
@@ -30,6 +22,7 @@ import java.util.HashMap;
 public class HoverTankGlowMain extends SimpleApplication {
 
     private final static String HOVER_TANK_MODEL = "Models/HoverTank/Tank.mesh.j3o";
+    SpotLight spot;
 
     public static void main(String[] args) {
         AppSettings settings = new AppSettings(true);
@@ -41,8 +34,8 @@ public class HoverTankGlowMain extends SimpleApplication {
     }
 
     private void initFloor() {
-        Box floorMesh = new Box(new Vector3f(-20,-2,-20), new Vector3f(20,-3,20));
-        floorMesh.scaleTextureCoordinates(new Vector2f(8, 8 ));
+        Box floorMesh = new Box(new Vector3f(-20, -2, -20), new Vector3f(20, -3, 20));
+        floorMesh.scaleTextureCoordinates(new Vector2f(8, 8));
         Geometry floorGeo = new Geometry("floor", floorMesh);
         Material floorMat = assetManager.loadMaterial("Textures/BrickWall/brickwall.j3m");//assetManager.loadMaterial("Textures/BrickWall/BrickWall.j3m")
 //        Material floorMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
@@ -75,19 +68,40 @@ public class HoverTankGlowMain extends SimpleApplication {
     }
 
     private void initLight() {
-        DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-1, -1, -2));
-        sun.setColor(ColorRGBA.White);
-        rootNode.addLight(sun);
-
+//        DirectionalLight sun = new DirectionalLight();
+//        sun.setDirection(new Vector3f(-1, -1, -2));
+//        sun.setColor(ColorRGBA.White);
+//        rootNode.addLight(sun);
 //        AmbientLight ambient = new AmbientLight();
 //        ambient.setColor(ColorRGBA.White);
 //        rootNode.addLight(ambient);
+        /**
+         * A white, spot light source.
+         */
+//        PointLight lamp = new PointLight();
+//        lamp.setPosition(new Vector3f(5,5,5));
+//        lamp.setColor(ColorRGBA.White);
+//        rootNode.addLight(lamp);
+        /**
+         * A cone-shaped spotlight with location, direction, range
+         */
+//        spot = new SpotLight();
+//        spot = new SpotLight();
+//        spot.setSpotRange(100);
+//        spot.setSpotOuterAngle(20 * FastMath.DEG_TO_RAD);
+//        spot.setSpotInnerAngle(15 * FastMath.DEG_TO_RAD);
+//        spot.setDirection(cam.getDirection());
+//        spot.setPosition(cam.getLocation());
+//        rootNode.addLight(spot);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        if (spot != null) {
+            spot.setDirection(cam.getDirection());
+            spot.setPosition(cam.getLocation());
+        }
+
     }
 
     @Override
