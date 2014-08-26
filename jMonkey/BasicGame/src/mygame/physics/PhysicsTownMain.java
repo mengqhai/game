a5901d6f1742a0bef953637e7c948d7ddb30a26c
Qinghai Meng <mengqhai@gmail.com>
2014-08-26 20:59:26 +0800

    physics town walk

--- jMonkey/BasicGame/src/mygame/physics/PhysicsTownMain.java
+++ jMonkey/BasicGame/src/mygame/physics/PhysicsTownMain.java
@@ -7,23 +7,37 @@ package mygame.physics;
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.plugins.ZipLocator;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.BetterCharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
+import com.jme3.scene.control.CameraControl;
 
 /**
  *
  * @author qinghai
  */
-public class PhysicsTownMain extends SimpleApplication {
+public class PhysicsTownMain extends SimpleApplication implements ActionListener {
 
     private Node sceneNode;
-    
     private BulletAppState bulletAppState;
     private RigidBodyControl scenePhy;
+    private Node playerNode;
+    private BetterCharacterControl playerControl;
+    private CameraNode camNode;
+    private Vector3f walkDirection = new Vector3f(0, 0, 0);
+    private Vector3f viewDirection = new Vector3f(0, 0, 1);
+    private boolean rotateLeft = false, rotateRight = false, forward = false, back = false;
+    private float speed = 8;
 
     @Override
     public void simpleInitApp() {
@@ -46,17 +60,97 @@ public class PhysicsTownMain extends SimpleApplication {
         sun.setDirection((new Vector3f(1.4f, -1.4f, -1.4f)).normalizeLocal());
         sun.setColor(ColorRGBA.White);
         rootNode.addLight(sun);
-        
+
+        initPhysics();
+        initPlayer();
+        initCam();
+        initInput();
+    }
+    
+    private void initPhysics() {
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
-        
+
         scenePhy = new RigidBodyControl(0f);
         sceneNode.addControl(scenePhy);
         bulletAppState.getPhysicsSpace().add(sceneNode);
     }
-    
+
+    private void initPlayer() {
+        playerNode = new Node("the player");
+        playerNode.setLocalTranslation(new Vector3f(0, 6, 0));
+        rootNode.attachChild(playerNode);
+
+        playerControl = new BetterCharacterControl(1.5f, 4, 30f);
+        playerControl.setJumpForce(new Vector3f(0, 300, 0));
+        playerControl.setGravity(new Vector3f(0, -10, 0));
+        playerNode.addControl(playerControl);
+        bulletAppState.getPhysicsSpace().add(playerControl);
+    }
+
+    private void initCam() {
+        camNode = new CameraNode("CamNode", cam);
+        camNode.setControlDir(CameraControl.ControlDirection.SpatialToCamera);
+        camNode.setLocalTranslation(0, 4, -6);
+        Quaternion quat = new Quaternion();
+        quat.lookAt(Vector3f.UNIT_Z, Vector3f.UNIT_Y);
+        camNode.setLocalRotation(quat);
+        playerNode.attachChild(camNode);
+        camNode.setEnabled(true);
+        flyCam.setEnabled(false);
+    }
+
+    private void initInput() {
+        inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Back", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+
+        inputManager.addListener(this, "Left", "Right");
+        inputManager.addListener(this, "Forward", "Back", "Jump");
+
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        Vector3f modelForwardDir = playerNode.getWorldRotation().mult(Vector3f.UNIT_Z);
+        walkDirection.set(0,0,0);
+        if (forward) {
+            walkDirection.addLocal(modelForwardDir.mult(speed));
+        } else if (back) {
+            walkDirection.addLocal(modelForwardDir.mult(speed).negate());
+        }
+        
+        playerControl.setWalkDirection(walkDirection);
+        if (rotateLeft) {
+            Quaternion rotateL = new Quaternion().fromAngleAxis(FastMath.PI * tpf, Vector3f.UNIT_Y);
+            // multiplies this quaternion by a parameter vector. The
+            // result is stored in the supplied vector
+            rotateL.multLocal(viewDirection);
+        } else if (rotateRight) {
+            Quaternion rotateR = new Quaternion().fromAngleAxis(-FastMath.PI * tpf, Vector3f.UNIT_Y);
+            rotateR.multLocal(viewDirection);
+        }
+        playerControl.setViewDirection(viewDirection);
+    }
+
     public static void main(String[] args) {
         PhysicsTownMain app = new PhysicsTownMain();
         app.start();
+    }
+
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals("Left")) {
+            rotateLeft = isPressed;
+        } else if (name.equals("Right")) {
+            rotateRight = isPressed;
+        } else if (name.equals("Forward")) {
+            forward = isPressed;
+        } else if (name.equals("Back")) {
+            back = isPressed;
+        } else if (name.equals("Jump")) {
+            playerControl.jump();
+        }
     }
 }
